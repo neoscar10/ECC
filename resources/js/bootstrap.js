e8ad707494fa6981f0751cbaca57d6efec19c258
neoscar <neoscar10@gmail.com>
2026-01-24 18:36:18 +0100

    built assets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,12 +7,6 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-
-    // If host is empty (normal for Pusher SaaS), Echo will use Pusher defaults.
-    wsHost: import.meta.env.VITE_PUSHER_HOST || undefined,
-    wsPort: Number(import.meta.env.VITE_PUSHER_PORT || 443),
-    wssPort: Number(import.meta.env.VITE_PUSHER_PORT || 443),
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME || 'https') === 'https',
-
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
