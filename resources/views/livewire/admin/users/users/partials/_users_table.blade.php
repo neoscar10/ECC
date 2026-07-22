347950994f59e2027809c81816146f1d60365434
neoscar <neoscar10@gmail.com>
2026-07-22 08:23:13 +0100

    Use empty instead of is_null for phone_verified_at in blade template

--- resources/views/livewire/admin/users/users/partials/_users_table.blade.php
+++ resources/views/livewire/admin/users/users/partials/_users_table.blade.php
@@ -99,7 +99,7 @@
                                         @if($user->currentMembership)
                                             <li><a class="dropdown-item" href="#" wire:click.prevent="$dispatch('open-update-tier-modal', { membershipId: {{ $user->currentMembership->id }} })"><i class="mdi mdi-swap-vertical align-bottom me-2 text-muted"></i> Update Tier</a></li>
                                         @endif
-                                        @if(is_null($user->phone_verified_at))
+                                        @if(empty($user->phone_verified_at))
                                             <li><a class="dropdown-item" wire:click="verifyUser({{ $user->id }})"><i class="ri-check-double-line align-bottom me-2 text-muted"></i> Verify Account</a></li>
                                         @endif
                                         @unless($user->hasRole('super_admin'))
